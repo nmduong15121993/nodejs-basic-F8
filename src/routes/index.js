ce5431bfebe3053e6f5dee3972df9eef9d187181
Duong Nguyen <nmduong15121993@gmail.com>
2021-10-26 22:06:31 +0700

    update final

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const newRouter = require('./news.route');
 const siteRouter = require('./site.route');
 
 function route(app) {
-        app.use('/news', newRouter);
-      app.use('/', siteRouter);
+  app.use('/news', newRouter);
+  app.use('/', siteRouter);
 }
 
 module.exports = route;
