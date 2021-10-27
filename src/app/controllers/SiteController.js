091a5e1a7f7c45e8b5a2b081b1817c176c707582
Duong Nguyen <nmduong15121993@gmail.com>
2021-10-27 10:16:23 +0700

    final

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,16 @@
 const Courses = require('../models/course.model');
-
+const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
   // [GET] /
-  index(req, res) {
+  index(req, res, next) {
+    // Courses.find({}, function(err, courses) {
+    //   if(!err) return res.json(courses);
+    //   res.status(400).json({ error: 'ERROR !!!' });
+    // })
 
-    Courses.find({}, function(err, courses) {
-      if(!err) return res.json(courses);
-      res.status(400).json({ error: 'ERROR !!!' });
-    })
-
+    Courses.find({})
+      .then((courses) => res.render('home', { title: 'Khoa Hoc', data: multipleMongooseToObject(courses)}))
+      .catch(next)
     // res.render('home');
   }
 
